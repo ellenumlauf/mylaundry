a9117f2541fa1febf763da61d84b3fe494fd1a0d
ellenumlauf <uml250736@spengergasse.at>
2026-05-23 19:58:13 +0200

    Layout of Home-, PricesView

--- src/main/java/at/spengergasse/views/home/HomeView.java
+++ src/main/java/at/spengergasse/views/home/HomeView.java
@@ -4,6 +4,8 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -22,44 +24,70 @@ public class HomeView extends VerticalLayout {
 
         setAlignItems(Alignment.CENTER);
 
-        H1 companyName = new H1 ("S U P E R C L E A N");
-        companyName.getStyle()
-            .set("font-family", "bold")
-            .set("font-size", "6rem")
-            .set("margin", "0");
-
-        H2 subName = new H2("The Whiter Than White Laundry");
-        subName.getStyle()
-            .set("margin", "0")
-            .set("color", "blue");
+        VerticalLayout header = getHeader();
+        FlexLayout totalInfo = new FlexLayout();
 
         Image logo = new Image("images/logo.png", "Logo");
         logo.setWidth("400px");
+        logo.setHeight("auto");
 
+        VerticalLayout info = new VerticalLayout();
         Paragraph line1 = new Paragraph("At our laundry shop, excellence is not just a promise — it is the standard we deliver every single day. From everyday garments to premium fabrics, every piece is treated with precision, care, and professional attention. Customers especially love our renowned Quick White Shirt Service, designed for busy professionals who expect perfectly cleaned, crisply pressed white shirts in record time without compromising quality. Every shirt leaves our shop looking immaculate, fresh, and ready to impress.");
         Paragraph line2 = new Paragraph ("Our Leather Jacket Service has become one of the most trusted specialty treatments in the area. Leather requires expert handling, and our experienced team uses premium cleaning and conditioning techniques to restore softness, shine, and elegance while protecting the material’s durability. Whether it is a luxury fashion piece or a beloved vintage jacket, customers consistently praise the remarkable transformation and attention to detail that make their garments look almost brand new again.");
         Paragraph line3 = new Paragraph ("What truly sets our laundry shop apart is our dedication to customer satisfaction, speed, and premium garment care. We combine modern cleaning technology with personalized service to create an experience that feels both luxurious and reliable. From spotless white shirts delivered with exceptional speed to expertly restored leather jackets, our shop has earned a reputation for quality, professionalism, and outstanding results that customers proudly recommend again and again.");
-        line1.setWidth("800px");
+        //line1.setWidth("400px");
+
         line1.getStyle()
             .set("font-size", "18px")
             .set("line-height", "1.4")
             .set("text-align", "left");
-        line2.setWidth("800px");
+        //line2.setWidth("300px");
         line2.getStyle()
             .set("font-size", "18px")
             .set("line-height", "1.4")
             .set("text-align", "left");
-        line3.setWidth("800px");
+        //line3.setWidth("400px");
         line3.getStyle()
             .set("font-size", "18px")
             .set("line-height", "1.4")
             .set("text-align", "left");
 
+        info.add(line1, line2, line3);
+        info.setMaxWidth("600px");
+        info.setWidthFull();
+
         H3 name = new H3("Superclean GmbH");
         H3 street = new H3("An der Alten Donau 1");
         H3 city = new H3("1020 Wien");
 
-        add(companyName, subName, logo, line1, line2, line3, name, street, city);
-    }
+        totalInfo.add(logo, info);
+        totalInfo.setWidthFull();
+        totalInfo.setJustifyContentMode(JustifyContentMode.CENTER);
+        totalInfo.setAlignItems(FlexComponent.Alignment.START);
+        totalInfo.setFlexWrap(FlexLayout.FlexWrap.WRAP);
+        add(totalInfo);
 
+        add(header, totalInfo, name, street, city);
+    }
+    public static VerticalLayout getHeader() {
+        VerticalLayout header;
+        header = new VerticalLayout();
+
+        header.setAlignItems(Alignment.CENTER);
+
+        H1 companyName = new H1("SUPERCLEAN");
+        companyName.getStyle()
+                .set("font-family", "bold")
+                .set("font-size", "6rem")
+                .set("margin", "0");
+
+        H2 subName = new H2("The Whiter Than White Laundry");
+        subName.getStyle()
+                .set("margin", "0")
+                .set("color", "blue");
+
+        header.add(companyName, subName);
+
+        return header;
+    }
 }
--- src/main/java/at/spengergasse/views/prices/PricesView.java
+++ src/main/java/at/spengergasse/views/prices/PricesView.java
@@ -1,9 +1,13 @@
 package at.spengergasse.views.prices;
 
+import at.spengergasse.views.home.HomeView;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
@@ -19,63 +23,54 @@ public class PricesView extends VerticalLayout {
     public PricesView() {
         setSpacing(false);
 
-        H1 companyName = new H1 ("S U P E R C L E A N");
-        companyName.getStyle()
-            .set("font-family", "bold")
-            .set("font-size", "6rem")
-            .set("margin", "0");
-
-        H2 subName = new H2("The Whiter Than White Laundry");
-        subName.getStyle()
-            .set("margin", "0")
-            .set("color", "blue");
+        setAlignItems(Alignment.CENTER);
+        VerticalLayout header = HomeView.getHeader();
+        setAlignItems(Alignment.CENTER);
 
         H2 priceList = new H2("Price List");
         priceList.getStyle()
             .set("margin", "0")
             .set("color", "blue");
 
-        H2 Trousers = new H2("Trousers");
-        Paragraph priceTrousers = new Paragraph("Cleaning: 15 €");
-        Paragraph priceTrousersIron = new Paragraph("Ironing: 8 €");
-        Paragraph priceFastT = new Paragraph("Express 24H Service: 10 €");
+        FlexLayout laundry = new FlexLayout();
 
-        H2 Dress = new H2("Dress");
-        Paragraph priceDress = new Paragraph("Cleaning: 20 €");
-        Paragraph priceDressIron = new Paragraph("Ironing: 11 €");
-        Paragraph priceFastD = new Paragraph("Express 24H Service: 11 €");
+        VerticalLayout tr = getCard("Trousers", 15, "Ironing", 8, 10 );
+        VerticalLayout dr = getCard("Dress", 20, "Ironing", 11, 11);
+        VerticalLayout sk = getCard("Skirt", 17, "Ironing", 9, 9);
+        VerticalLayout ju = getCard("Jumper", 14, "Ironing", 10, 8);
+        VerticalLayout lj = getCard("Leatherjacket", 95, "Impregnation", 25, 20);
 
-        H2 Skirt = new H2("Skirt");
-        Paragraph priceSkirt = new Paragraph("Cleaning: 17 €");
-        Paragraph priceSkirtIron = new Paragraph("Ironing: 9 €");
-        Paragraph priceFastS = new Paragraph("Express 24H Service: 9 €");
+        laundry.add(tr, dr, sk, ju, lj);
+        laundry.setWidthFull();
+        laundry.setJustifyContentMode(JustifyContentMode.CENTER);
+        laundry.setFlexWrap(FlexLayout.FlexWrap.WRAP);
+        add(laundry);
 
-        H2 Jumper = new H2("Jumper");
-        Paragraph priceJumper = new Paragraph("Cleaning: 14 €");
-        Paragraph priceJumperIron = new Paragraph("Ironing: 10 €");
-        Paragraph priceFastJ = new Paragraph("Express 24H Service: 8 €");
 
-        H2 Leatherjacket = new H2("Leatherjacket");
-        Paragraph priceLeatherjacket = new Paragraph("Cleaning: 95 €");
-        Paragraph priceLJImpreg = new Paragraph("Impregnation: 25 €");
-        Paragraph priceFastLJ = new Paragraph("Express 24H Service: 20 €");
-        /*companyName, subName, priceList, Trousers,*/
-        add(companyName, subName, priceList, Trousers, priceTrousers, priceTrousersIron, priceFastT,
-                Dress, priceDress, priceDressIron, priceFastD,
-                Skirt, priceSkirt, priceSkirtIron, priceFastS,
-                Jumper, priceJumper, priceJumperIron, priceFastJ,
-                Leatherjacket, priceLeatherjacket, priceLJImpreg, priceFastLJ);
+        Paragraph info = new Paragraph("includes taxes. Delivery is also possible");
 
-        /*H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-        */
-        /*
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center"); */
+        add(header, priceList, laundry, info);
     }
+
+    public VerticalLayout getCard(String laundryName, double price, String extraService, double priceIroningImp, double priceExpress) {
+        VerticalLayout whichLaundry = new VerticalLayout();
+        H2 laundryNameH2 = new H2(laundryName);
+        Paragraph laundryPrice = new Paragraph("Cleaning: " + price + " €");
+        Paragraph priceII = new Paragraph(extraService + ": " + priceIroningImp + " €");
+        Paragraph priceExp = new Paragraph("Express 24H Service: " + priceExpress + " €");
+
+        whichLaundry.setWidth("350px");
+        whichLaundry.setPadding(true);
+        whichLaundry.setSpacing(false);
+
+        whichLaundry.getStyle()
+            .set("border", "1px solid blue")
+            .set("border-radius", "10px")
+            .set("margin", "10px");
+        whichLaundry.add(laundryNameH2, laundryPrice, priceII, priceExp);
+
+        return whichLaundry;
+    }
+
 
 }
